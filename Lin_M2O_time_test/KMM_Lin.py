87e743fc468371edddefe67d33896922702c19c5
GiLonn <114254156+GiLonn@users.noreply.github.com>
2023-11-22 01:59:27 +0800

    Add files via upload

---
+++ Lin_M2O_time_test/KMM_Lin.py
@@ -0,0 +1,89 @@
+import numpy as np
+import sklearn.metrics
+from cvxopt import matrix, solvers
+import os
+from PIL import Image, ImageFilter
+import cv2
+
+def compute_kmm(source_positive_dir_path,target_positive_dir_path):
+    openDirectory = os.getcwd()
+    # source_positive_dir_path = os.path.join(openDirectory, "datasets", "after_elite_sample", fruit_name, "source","train", "positive")
+    # target_positive_dir_path = os.path.join(openDirectory, "datasets", "after_elite_sample", fruit_name, "target","train","positive")
+    data_name_sp = os.listdir(source_positive_dir_path)
+    data_name_tp = os.listdir(target_positive_dir_path)
+    for j in range(len(data_name_sp)):
+        path_now = os.path.join(source_positive_dir_path, data_name_sp[j])
+        if j == 0:
+            KMM_source_positive_sample = openimage(path_now).reshape((1, -1))
+        elif j > 0:
+            KMM_source_positive_sample = np.append(KMM_source_positive_sample, openimage(path_now).reshape((1, -1)),axis=0)
+    for k in range(len(data_name_tp)):
+        path_now = os.path.join(target_positive_dir_path, data_name_tp[k])
+        if k == 0:
+            KMM_target_positive_sample = openimage(path_now).reshape((1, -1))
+        elif k > 0:
+            KMM_target_positive_sample = np.append(KMM_target_positive_sample, openimage(path_now).reshape((1, -1)),axis=0)
+    KMM_source_positive_sample, KMM_target_positive_sample = np.asarray(KMM_source_positive_sample), np.asarray(KMM_target_positive_sample)
+    KMM_source_positive_sample = KMM_source_positive_sample.astype(np.double)
+    KMM_target_positive_sample = KMM_target_positive_sample.astype(np.double)
+
+    kmm = KMM(kernel_type='rbf',B=2)
+    beta = kmm.fit(KMM_source_positive_sample, KMM_target_positive_sample)
+    return beta[0]
+
+def openimage(path):
+    return (cv2.cvtColor(cv2.resize(cv2.imread(path),(224,224)),cv2.COLOR_BGR2RGB)).flatten()
+
+def kernel(ker, X1, X2, gamma):
+    K = None
+    if ker == 'linear':
+        if X2 is not None:
+            # Compute the linear kernel between X1 and X2, Gram matrix array of shape (X1_samples, X2_samples)
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1), np.asarray(X2))
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1))
+    elif ker == 'rbf':
+        if X2 is not None:
+            # Compute the rbf (gaussian) kernel between X1 and X2, Gram matrix array of shape (X1_samples, X2_samples)
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1), np.asarray(X2), gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1), None, gamma)
+    return K
+
+class KMM:
+    def __init__(self, kernel_type='rbf', gamma=1.0, B=2.0, eps=None):
+        '''
+        Initialization function
+        :param kernel_type: 'linear' | 'rbf'
+        :param gamma: kernel bandwidth for rbf kernel, like K(x, y) = exp(-gamma ||x-y||^2), If None, defaults to 1.0 / n_features
+        :param B: bound for beta
+        :param eps: bound for sigma_beta
+        '''
+        self.kernel_type = kernel_type
+        self.gamma = gamma
+        self.B = B
+        self.eps = eps
+
+    def fit(self, Xs, Xt):
+        '''
+        Fit source and target using KMM (compute the coefficients)
+        :param Xs: ns * dim
+        :param Xt: nt * dim
+        :return: Coefficients (Pt / Ps) value vector (Beta in the paper)
+        '''
+        ns = Xs.shape[0]
+        nt = Xt.shape[0]
+        if self.eps == None:
+            self.eps = self.B / np.sqrt(ns)
+        K = kernel(self.kernel_type, Xs, None, self.gamma)
+        kappa = np.sum(kernel(self.kernel_type, Xs, Xt, self.gamma) * float(ns) / float(nt), axis=1)
+
+        K = matrix(K)
+        kappa = matrix(kappa)
+        G = matrix(np.r_[np.ones((1, ns)), -np.ones((1, ns)), np.eye(ns), -np.eye(ns)])
+        h = matrix(np.r_[ns * (1 + self.eps), ns * (self.eps - 1), self.B * np.ones((ns,)), np.zeros((ns,))])
+
+        sol = solvers.qp(K, -kappa, G, h)
+        beta = np.array(sol['x'])
+        return beta
+
---
+++ M2o 3_to_1_pi_version/M2O_LXZ/3_to_1/mfsan_3_1_1.py
@@ -0,0 +1,324 @@
+from __future__ import print_function
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import os
+import math
+import data_loader
+import resnet as models
+import numpy as np
+
+import KMM_Lin
+from sklearn.metrics import confusion_matrix
+
+import argparse
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Unsupported value encountered.')
+
+parser = argparse.ArgumentParser(description="M2O_3_to_1_train")
+# parser.add_argument("--preprocess", type=str2bool, default=False, help='run prepare_data or not')
+
+parser.add_argument("--data_path",type=str, default="None",help='path to training data')
+parser.add_argument("--batch_size", type=int, default=8, help="Training batch size")
+parser.add_argument("--lr", type=float, default=0.0001, help="initial learning rate")
+# parser.add_argument("--lambda_", type=float, default=None, help="lambda of MMD or Coral")
+parser.add_argument("--iteration", type=int, default=1000, help="Number of training epochs")
+# parser.add_argument("--kmm_source1_path", type=str, default="None", help="kmm_source1_path")
+# parser.add_argument("--kmm_source2_path", type=str, default="None", help="kmm_source2_path")
+# parser.add_argument("--kmm_target_path", type=str, default="None", help="kmm_target_path")
+parser.add_argument("--save_model_path_name", type=str, default="None", help='path to save models and log files')
+parser.add_argument("--save_test_name", type=str, default='test_log1.csv', help='path to save models and log files')
+parser.add_argument("--save_train_loss_name1", type=str, default='train_log1.csv', help='path to save models and log files')
+parser.add_argument("--save_train_loss_name2", type=str, default='train_log2.csv', help='path to save models and log files')
+parser.add_argument("--save_train_loss_name3", type=str, default='train_log3.csv', help='path to save models and log files')
+# parser.add_argument("--use_gpu", type=str2bool, default=True, help='use GPU or not')
+parser.add_argument("--gpu_id", type=str, default='cuda:0', help='GPU id')
+
+opt = parser.parse_args()
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+torch.cuda.set_device(opt.gpu_id)
+logtrain1 = [] #創建一個空的list
+logtrain2 = [] #創建一個空的list
+logtrain3 = [] #創建一個空的list
+logtest = [] #創建一個空的list
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# Training settings
+# batch_size = 8
+# iteration = 150
+# lr = 0.0001
+# momentum = 0.9
+cuda = True
+seed = 8
+log_interval = 1
+l2_decay = 1e-4
+betas = [0.9, 0.999]
+iteration = opt.iteration
+
+root_path = opt.data_path
+source1_name = "\\source1\\train"
+source2_name = '\\source2\\train'
+source3_name = '\\source3\\train'
+target_train_name = "\\target\\train"
+target_test_name = "\\target\\test"
+
+torch.manual_seed(seed)
+if cuda:
+    torch.cuda.manual_seed(seed)
+
+kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+
+source1_loader = data_loader.load_training(root_path, source1_name, opt.batch_size, kwargs)
+source2_loader = data_loader.load_training(root_path, source2_name, opt.batch_size, kwargs)
+source3_loader = data_loader.load_training(root_path, source3_name, opt.batch_size, kwargs)
+target_train_loader = data_loader.load_training(root_path, target_train_name, opt.batch_size, kwargs)
+target_test_loader = data_loader.load_testing(root_path, target_test_name, opt.batch_size, kwargs)
+
+def train(model):
+    source1_iter = iter(source1_loader)
+    source2_iter = iter(source2_loader)
+    source3_iter = iter(source3_loader)
+    target_iter = iter(target_train_loader)
+    correct = 0
+
+    optimizer = torch.optim.Adam([
+            {'params': model.sharedNet.parameters()},
+            {'params': model.cls_fc_son1.parameters(), 'lr': opt.lr},
+            {'params': model.cls_fc_son2.parameters(), 'lr': opt.lr},
+            {'params': model.cls_fc_son3.parameters(), 'lr': opt.lr},
+            {'params': model.sonnet1.parameters(), 'lr': opt.lr},
+            {'params': model.sonnet2.parameters(), 'lr': opt.lr},
+            {'params': model.sonnet3.parameters(), 'lr': opt.lr},
+        ], lr=opt.lr, betas=betas, weight_decay=l2_decay)
+
+    scheduler=torch.optim.lr_scheduler.StepLR(optimizer,step_size=10,gamma=0.25)
+
+    for i in range(1, iteration + 1):
+        model.train()
+        # LEARNING_RATE = lr / math.pow((1 + 10 * (i - 1) / (iteration)), 0.75)
+        # if (i - 1) % 100 == 0:
+        #     print("learning rate：", LEARNING_RATE)
+        # optimizer = torch.optim.SGD([
+        #     {'params': model.sharedNet.parameters()},
+        #     {'params': model.cls_fc_son1.parameters(), 'lr': LEARNING_RATE},
+        #     {'params': model.cls_fc_son2.parameters(), 'lr': LEARNING_RATE},
+        #     {'params': model.cls_fc_son3.parameters(), 'lr': LEARNING_RATE},
+        #     {'params': model.sonnet1.parameters(), 'lr': LEARNING_RATE},
+        #     {'params': model.sonnet2.parameters(), 'lr': LEARNING_RATE},
+        #     {'params': model.sonnet3.parameters(), 'lr': LEARNING_RATE},
+        # ], lr=LEARNING_RATE / 10, momentum=momentum, weight_decay=l2_decay)
+
+        
+
+
+
+        try:
+            source_data, source_label = source1_iter.next()
+        except Exception as err:
+            source1_iter = iter(source1_loader)
+            source_data, source_label = source1_iter.next()
+        try:
+            target_data, __ = target_iter.next()
+        except Exception as err:
+            target_iter = iter(target_train_loader)
+            target_data, __ = target_iter.next()
+        if cuda:
+            source_data, source_label = source_data.cuda(), source_label.cuda()
+            target_data = target_data.cuda()
+        source_data, source_label = Variable(source_data), Variable(source_label)
+        target_data = Variable(target_data)
+        optimizer.zero_grad()
+
+        cls_loss, mmd_loss, l1_loss = model(source_data, target_data, source_label, mark=1)
+        gamma = 2 / (1 + math.exp(-10 * (i) / (iteration) )) - 1
+        # loss = cls_loss + gamma * (mmd_loss + l1_loss)
+        loss = KMM_weight_source1 * cls_loss + gamma * mmd_loss
+        loss.backward()
+        optimizer.step()
+
+        if i % (log_interval*2) == 0:
+            print('Train source1 iter: {} [({:.0f}%)]\tLoss: {:.6f}\tsoft_Loss: {:.6f}\tmmd_Loss: {:.6f}\tl1_Loss: {:.6f}'.format(
+                i, 100. * i / iteration, loss.item(), cls_loss.item(), mmd_loss.item(), l1_loss.item()))
+
+        logtrain1.append([loss, cls_loss,KMM_weight_source1*cls_loss,mmd_loss,gamma*mmd_loss])
+        np_log1 = np.array(logtrain1, dtype=float)
+        np.savetxt(opt.save_train_loss_name1, np_log1, delimiter=',', fmt='%.12f')
+
+
+        try:
+            source_data, source_label = source2_iter.next()
+        except Exception as err:
+            source2_iter = iter(source2_loader)
+            source_data, source_label = source2_iter.next()
+        try:
+            target_data, __ = target_iter.next()
+        except Exception as err:
+            target_iter = iter(target_train_loader)
+            target_data, __ = target_iter.next()
+        if cuda:
+            source_data, source_label = source_data.cuda(), source_label.cuda()
+            target_data = target_data.cuda()
+        source_data, source_label = Variable(source_data), Variable(source_label)
+        target_data = Variable(target_data)
+        optimizer.zero_grad()
+
+        cls_loss, mmd_loss, l1_loss = model(source_data, target_data, source_label, mark=2)
+        gamma = 2 / (1 + math.exp(-10 * (i) / (iteration))) - 1
+        # loss = cls_loss + gamma * (mmd_loss + l1_loss)
+        loss = KMM_weight_source2 * cls_loss + gamma * mmd_loss
+
+        loss.backward()
+        optimizer.step()
+
+        if i % (log_interval*2) == 0:
+            print(
+                'Train source2 iter: {} [({:.0f}%)]\tLoss: {:.6f}\tsoft_Loss: {:.6f}\tmmd_Loss: {:.6f}\tl1_Loss: {:.6f}'.format(
+                    i, 100. * i / iteration, loss.item(), cls_loss.item(), mmd_loss.item(), l1_loss.item()))
+
+        logtrain2.append([loss, cls_loss,KMM_weight_source2*cls_loss,mmd_loss,gamma*mmd_loss])
+        np_log2 = np.array(logtrain2, dtype=float)
+        np.savetxt(opt.save_train_loss_name2, np_log2, delimiter=',', fmt='%.12f')
+
+        try:
+            source_data, source_label = source3_iter.next()
+        except Exception as err:
+            source3_iter = iter(source3_loader)
+            source_data, source_label = source3_iter.next()
+        try:
+            target_data, __ = target_iter.next()
+        except Exception as err:
+            target_iter = iter(target_train_loader)
+            target_data, __ = target_iter.next()
+        if cuda:
+            source_data, source_label = source_data.cuda(), source_label.cuda()
+            target_data = target_data.cuda()
+        source_data, source_label = Variable(source_data), Variable(source_label)
+        target_data = Variable(target_data)
+        optimizer.zero_grad()
+
+        cls_loss, mmd_loss, l1_loss = model(source_data, target_data, source_label, mark=3)
+        gamma = 2 / (1 + math.exp(-10 * (i) / (iteration))) - 1
+        # loss = cls_loss + gamma * (mmd_loss + l1_loss)
+        loss = KMM_weight_source3 * cls_loss + gamma * mmd_loss
+
+        loss.backward()
+        optimizer.step()
+
+        scheduler.step()
+        # print(scheduler.get_lr())
+
+        if i % (log_interval*2) == 0:
+            print(
+                'Train source3 iter: {} [({:.0f}%)]\tLoss: {:.6f}\tsoft_Loss: {:.6f}\tmmd_Loss: {:.6f}\tl1_Loss: {:.6f}'.format(
+                    i, 100. * i / iteration, loss.item(), cls_loss.item(), mmd_loss.item(), l1_loss.item()))
+
+
+        if i % log_interval == 0:
+            t_correct = test(model)
+            if t_correct > correct:
+                correct = t_correct
+            print(source1_name, source2_name, source3_name, "to", target_test_name, "%s max correct:" % target_test_name, correct.item(), "\n")
+
+        logtrain3.append([loss, cls_loss,KMM_weight_source3*cls_loss,mmd_loss,gamma*mmd_loss])
+        np_log3 = np.array(logtrain3, dtype=float)
+        np.savetxt(opt.save_train_loss_name3, np_log3, delimiter=',', fmt='%.12f')
+
+
+def test(model):
+    model.eval()
+    test_loss = 0
+    correct_test = 0.
+    correct1 = 0
+    correct2 = 0
+    correct3 = 0
+    count_stack = 0
+    with torch.no_grad():
+        for data, target in target_test_loader:
+            if cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            pred1, pred2, pred3 = model(data)
+
+            pred1 = torch.nn.functional.softmax(pred1, dim=1)
+            pred2 = torch.nn.functional.softmax(pred2, dim=1)
+            pred3 = torch.nn.functional.softmax(pred3, dim=1)
+
+            pred = (pred1 + pred2 + pred3) / 3
+            test_loss += F.nll_loss(F.log_softmax(pred, dim=1), target).item()  # sum up batch loss
+
+            pred = pred.data.max(1)[1]  # get the index of the max log-probability
+            pred_total = pred
+            correct_test += pred.eq(target.data.view_as(pred)).cpu().sum()
+            
+            pred = pred1.data.max(1)[1]  # get the index of the max log-probability
+            correct1 += pred.eq(target.data.view_as(pred)).cpu().sum()
+            
+            pred = pred2.data.max(1)[1]  # get the index of the max log-probability
+            correct2 += pred.eq(target.data.view_as(pred)).cpu().sum()
+            
+            pred = pred3.data.max(1)[1]  # get the index of the max log-probability
+            correct3 += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+            pred_matrix = pred_total.data
+            target_matrix = target.data
+            if count_stack == 0:
+                pred_matrix_total = pred_matrix
+                target_matrix_total = target_matrix
+                count_stack =1
+            elif count_stack == 1:
+                pred_matrix_total = torch.cat((pred_matrix_total,pred_matrix))
+                target_matrix_total = torch.cat((target_matrix_total,target_matrix))
+
+        target_matrix_total = target_matrix_total.cpu().numpy()
+        pred_matrix_total = pred_matrix_total.cpu().numpy()
+        tn, fp, fn, tp = confusion_matrix(target_matrix_total, pred_matrix_total,labels=[0,1]).ravel()
+
+        test_loss /= len(target_test_loader.dataset)
+        print(target_test_name, '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+            test_loss, correct_test.type(torch.float32), len(target_test_loader.dataset),100. * correct_test.type(torch.float32) / len(target_test_loader.dataset)))
+        print('\nsource1 accnum {}, source2 accnum {}，source3 accnum {}'.format(correct1.type(torch.float32), correct2.type(torch.float32), correct3.type(torch.float32)))
+
+        test_total = 100*correct_test.type(torch.float32)/len(target_test_loader.dataset)
+
+        logtest.append([test_total,tn, fp, fn, tp,correct1,correct2,correct3])
+        np_log = np.array(logtest, dtype=float)
+        np.savetxt(opt.save_test_name, np_log, delimiter=',', fmt='%.6f')
+
+    return correct_test.type(torch.float32)
+
+if __name__ == '__main__':
+    model = models.MFSAN(num_classes=2)
+    print(model)
+    if cuda:
+        model.cuda()
+
+    kmm_source1_path = os.path.join(root_path,"source1","train","positive")
+    kmm_source2_path = os.path.join(root_path,"source2","train","positive")
+    kmm_source3_path = os.path.join(root_path,"source3","train","positive")
+    kmm_target_path = os.path.join(root_path,"target","train","positive")
+
+    KMM_weight_source1 = KMM_Lin.compute_kmm(kmm_source1_path,kmm_target_path)
+    KMM_weight_source2 = KMM_Lin.compute_kmm(kmm_source2_path,kmm_target_path)
+    KMM_weight_source3 = KMM_Lin.compute_kmm(kmm_source3_path,kmm_target_path)
+
+    KMM_weight_source1 = torch.from_numpy(KMM_weight_source1).float().to(DEVICE)
+    print(KMM_weight_source1)
+
+    KMM_weight_source2 = torch.from_numpy(KMM_weight_source2).float().to(DEVICE)
+    print(KMM_weight_source2)
+
+    KMM_weight_source3 = torch.from_numpy(KMM_weight_source3).float().to(DEVICE)
+    print(KMM_weight_source3)
+
+    train(model)
+
+    torch.save(model.state_dict(), opt.save_model_path_name)
