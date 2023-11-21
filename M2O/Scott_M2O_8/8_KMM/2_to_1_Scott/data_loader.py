48b6da4608cb9846c106c337005db8e39b4009e7
GiLonn <114254156+GiLonn@users.noreply.github.com>
2023-11-22 02:12:00 +0800

    Add files via upload

---
+++ M2O/Scott_M2O_8/8_KMM/2_to_1_Scott/data_loader.py
@@ -0,0 +1,27 @@
+from torchvision import datasets, transforms
+import torch
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    numpy.random.seed(worker_seed)
+    random.seed(worker_seed)
+g = torch.Generator()
+g.manual_seed(0)
+
+def load_training(root_path, dir, batch_size, kwargs):
+    transform = transforms.Compose(
+        [transforms.Resize([256, 256]),
+         transforms.RandomCrop(224),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor()])
+    data = datasets.ImageFolder(root=root_path + dir, transform=transform)
+    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True,worker_init_fn=seed_worker, generator = g,  **kwargs)
+    return train_loader
+
+def load_testing(root_path, dir, batch_size, kwargs):
+    transform = transforms.Compose(
+        [transforms.Resize([224, 224]),
+         transforms.ToTensor()])
+    data = datasets.ImageFolder(root=root_path + dir, transform=transform)
+    test_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=False,**kwargs)
+    return test_loader
